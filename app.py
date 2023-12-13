18182c37fba55e02051bf1b74984e37028f02bcb
slinhomes <142390550+slinhomes@users.noreply.github.com>
2023-12-13 15:29:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ _=load_dotenv(find_dotenv())
 def create_connection():
     server = 'studenthomesmgmt.database.windows.net'
     database = 'PortfolioManagement'  
-    username = os.getenv('USERNAME')
-    password = os.getenv('PASSWORD')
+    username = 'slin'
+    password = 'Sl2023!!'
     driver = '{ODBC Driver 17 for SQL Server}'  # Adjust the driver if needed
     conn = pyodbc.connect(
         f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}')
