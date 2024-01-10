8ddb0b12d414c2a947a635ff2e20eeb8b35f85e4
slinhomes <142390550+slinhomes@users.noreply.github.com>
2024-01-10 16:00:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,17 @@ import os
 from dotenv import load_dotenv, find_dotenv
 _=load_dotenv(find_dotenv())
 
-myusername = os.getenv("USERNAME")
-print(myusername)
-mypassword = os.getenv("PASSWORD")
-print(mypassword)
+#myusername = os.getenv("USERNAME")
+#print(myusername)
+#mypassword = os.getenv("PASSWORD")
+#print(mypassword)
 
 # Function to connect to the Azure SQL database
 def create_connection():
     server = 'studenthomesmgmt.database.windows.net'
     database = 'PortfolioManagement'  
-    username = myusername
-    password = mypassword
+    username = st.secrets['db_username']
+    password = st.secrets['db_password']
     driver = '{ODBC Driver 17 for SQL Server}'  # Adjust the driver if needed
     conn = pyodbc.connect(
         f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}')
